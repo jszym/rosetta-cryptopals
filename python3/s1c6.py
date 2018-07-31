efa0f18826909750f70a8b6e22b31ffd0545b85b
Joseph Szymborski <joseph.szymborski@naj-p.com>
2018-07-31 00:17:10 -0400

    Updated is_english to function to a much better observed frequency model like the one in S1C3

--- python3/s1c6.py
+++ python3/s1c6.py
@@ -5,7 +5,7 @@ import numpy as np
 from base64 import b64decode
 from s1c5 import repeating_xor
 from collections import Counter
-import random
+import pickle
 
 def hamming(x, y):
     """Calculate the Hamming distance between two bit strings"""
@@ -18,45 +18,17 @@ def hamming(x, y):
     return count
 
 def is_english(input_bin):
-    counts = Counter(input_bin.upper()).items()
+    counts = Counter(input_bin).items()
     chars = np.array([chr(c) for c, _ in counts])
     freqs = np.array([v for _, v in counts])/len(input_bin)
-    expected_freqs = [
-        ("A",8.167),
-        ("B",1.492),
-        ("C",2.782),
-        ("D",4.253),
-        ("E",12.702),
-        ("F",2.228),
-        ("G",2.015),
-        ("H",6.094),
-        ("I",6.966),
-        ("J",0.153),
-        ("K",0.772),
-        ("L",4.025),
-        ("M",2.406),
-        ("N",6.749),
-        ("O",7.507),
-        ("P",1.929),
-        ("Q",0.095),
-        ("R",5.987),
-        ("S",6.327),
-        ("T",9.056),
-        ("U",2.758),
-        ("V",0.978),
-        ("W",2.360),
-        ("X",0.150), 
-        ("Y",1.974),
-        ("Z",0.074)
-    ]
+    with open("../data/freq_en.pickle", "rb") as f:
+        expected_freqs = pickle.load(f)
 
     score = 1
 
     for letter, exp_freq in expected_freqs:
         if letter in chars:
-            score += abs((exp_freq/100) - freqs[list(chars).index(letter)])
-        elif letter == " ":
-            score += 0
+            score += abs((exp_freq) - freqs[list(chars).index(letter)])
         else:
             score += 1 
 
